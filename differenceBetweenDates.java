8998aa97ec7954f19ab74703e9e6cef3b91c9b80
Baldi00 <46602744+Baldi00@users.noreply.github.com>
2021-05-02 11:21:18 +0200

    Update differenceBetweenDates.java

--- differenceBetweenDates.java
+++ differenceBetweenDates.java
@@ -1,9 +1,10 @@
 try {
+    int dayInMillisec = 24 * 60 * 60 * 1000;
     SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
     Date firstDate = sdf.parse("2000-01-01");
     Date secondDate = sdf.parse("2000-12-31");
-    secondDate.setTime(secondDate.getTime()+24*60*60*1000-1); //To be more precise add 1day-1millisec (to have the entire last day)
-    int diffInDays = (int)((secondDate.getTime() - firstDate.getTime())/ (1000 * 60 * 60 * 24));
+    secondDate.setTime(secondDate.getTime() + dayInMillisec-1); //To be more precise add 1day-1millisec (to have the entire last day)
+    int diffInDays = (int)((secondDate.getTime() - firstDate.getTime())/dayInMillisec);
     System.out.println(diffInDays);
 }catch(ParseException e){
     System.err.println("Incorrect date format");
